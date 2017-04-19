7ec5a17389e26072296c6803f78a06d4693e442b
tomcat6 <tomcat6@tom1-core-app1>
2017-04-19 11:58:13 +0000

    Initialize app from template

---
+++ application.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var app   = require('./lib/app.js')
+  , http = require('http')
+  , port  = app.get('port')
+  , ip = app.get('base url')
+  ;
+
+var tag = 'SERVER';
+
+var server = http.createServer(app);
+server.listen(port, ip, function() {
+  console.log("App started at: " + new Date() + " on port: " + port);
+});
